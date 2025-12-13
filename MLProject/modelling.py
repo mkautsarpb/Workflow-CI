14ae2bef52eac72924c41f220c3a0ea4950e9016
Muhammad Kautsar Putra Binoba <mkautsar292004@gmail.com>
2025-12-14 05:59:19 +0700

    Add MLProject for CI retraining

---
+++ MLProject/modelling.py
@@ -0,0 +1,103 @@
+import os
+import pandas as pd
+import mlflow
+import dagshub
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+import joblib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(
+    BASE_DIR,
+    "heart_disease_preprocessing",
+    "heart_disease_clean.csv"
+)
+
+# =========================
+# DAGSHUB (MLFLOW ONLINE)
+# =========================
+dagshub.init(
+    repo_owner="mkautsarpb",
+    repo_name="Eksperimen_SML_Muhammad_Kautsar_Putra_Binoba",
+    mlflow=True
+)
+
+mlflow.set_experiment("Heart Disease - Tuning Model")
+
+# =========================
+# LOAD DATA
+# =========================
+df = pd.read_csv(DATA_PATH)
+
+X = df.drop(columns=["target"])
+y = df["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# =========================
+# HYPERPARAMETER TUNING
+# =========================
+param_grid = {
+    "C": [0.01, 0.1, 1, 10],
+    "solver": ["liblinear"]
+}
+
+grid = GridSearchCV(
+    LogisticRegression(max_iter=1000),
+    param_grid,
+    cv=5,
+    scoring="accuracy"
+)
+
+with mlflow.start_run():
+    grid.fit(X_train, y_train)
+    best_model = grid.best_estimator_
+
+    y_pred = best_model.predict(X_test)
+
+    # =========================
+    # MANUAL LOGGING (METRICS)
+    # =========================
+    mlflow.log_params(grid.best_params_)
+
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("precision", prec)
+    mlflow.log_metric("recall", rec)
+
+    # =========================
+    # ARTEFAK 1: MODEL
+    # =========================
+    model_path = os.path.join(BASE_DIR, "model.joblib")
+    joblib.dump(best_model, model_path)
+    mlflow.log_artifact(model_path)
+
+    # =========================
+    # ARTEFAK 2: CONFUSION MATRIX
+    # =========================
+    cm = confusion_matrix(y_test, y_pred)
+
+    plt.figure(figsize=(5, 4))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+    plt.title("Confusion Matrix")
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+
+    cm_path = os.path.join(BASE_DIR, "confusion_matrix.png")
+    plt.savefig(cm_path)
+    plt.close()
+
+    mlflow.log_artifact(cm_path)
+
+    print("Accuracy:", acc)
+    print("Precision:", prec)
+    print("Recall:", rec)
