ef4937dcf47507c9272072f4ff3bbe17ccb22a56
Muhammad Kautsar Putra Binoba <mkautsar292004@gmail.com>
2025-12-14 07:46:13 +0700

    ix: use nested mlflow run for MLflow Project compatibility

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -21,7 +21,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-with mlflow.start_run():
+with mlflow.start_run(nested=True):
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
 
